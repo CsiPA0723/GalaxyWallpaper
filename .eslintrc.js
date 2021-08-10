5bd2e5fa08481895f1b3fd0f4928a2bf4e09b3c1
Péter Csizmadia <csizipeti0723@gmail.com>
2021-08-10 22:22:50 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-  ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
   },
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "auto",
+};
